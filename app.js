1049fcae51cb5206c2ff33d27917cda27aa9bfb0
Арсений Попов <tatyanka.popova.1970@inbox.ru>
2023-04-09 16:38:47 +0300

    cursor. выводит данные из таблиц в консоль, не вылетает, но не знаю как их преобразовать

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ const users = require('./routes/users');
 const todoRoutes = require('./routes/routes');
 const app = express();
 
+
+
+
+
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
     helpers: {
+
+
         substr: function (length, context, options) {
             if (context.length > length) {
                 return context.substring(0, length) + "...";
@@ -24,6 +31,8 @@ const hbs = exphbs.create({
                 return context;
             }
         },
+
+
         getTime: function () {
             var myDate = new Date()
             var hour = myDate.getHours()
@@ -36,11 +45,69 @@ const hbs = exphbs.create({
                 second = '0' + second
             }
             return (
-                '������� �����: ' + hour + ':' + minute + ':' + second
+                'Текущее время: ' + hour + ':' + minute + ':' + second
             )
+        },
+
+
+        connect: function (table) {
+            let table2 = 'departments';
+            //query = hbs.escapeExpression(query);      //экранирование выражения
+            let query = ` SELECT * FROM `+table;
+            const { Pool } = require('pg');
+            const Cursor = require('pg-cursor');
+            let mass = 'V';
+            console.log(table + ' ж');
+
+            const pool = new Pool({
+                user: 'postgres',
+                host: 'localhost',
+                database: 'LNK',
+                password: '4444',
+                port: 5432,
+            });
+
+            (async () => {
+                const client = await pool.connect();
+                
+
+                const cursor = await client.query(new Cursor(query));
+                var pkjo = 0;
+                function read(rws,cb) {
+                    cursor.read(rws, (err, rows) => {
+                        
+                        console.log(rows);
+                        mass = mass + rows[0,0];
+                        if (rows != 0) {
+                            
+                            read(1,cb);
+                        }
+                        pkjo++;
+                        /*console.log(Object.keys(cursor.rows?.[0]).join('ccc'));*/
+                        
+                    });
+                    cb(pkjo,mass);
+                    
+                }
+                console.log('dsf');
+                function cons(str1,str2) {
+                    
+                        console.log(str1+'э');
+                        console.log(str2 + ' (*_');                    
+                }
+                read(5, cons);                
+            })();            
         }
+
+
     }
 });
+
+
+
+
+
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
