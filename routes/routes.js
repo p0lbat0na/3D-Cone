bfb02273fbf2016ff6920c54ec087e04012923af
Арсений Попов <tatyanka.popova.1970@inbox.ru>
2023-04-16 15:56:55 +0300

    жжжж

--- routes/routes.js
+++ routes/routes.js
@@ -16,11 +16,41 @@ router.get('/req-list', (req, res) => {
     })
 })
 
+router.get('/test-list', (req, res) => {
+    res.render('test-list', {
+        title: 'Испытания',
+        isTestList: true
+    })
+})
+
 router.get('/create-req', (req, res) => {
     res.render('create-req', {
         title: 'Новая заявка',
         isNewReq: true
     })
 })
+
+router.get('/catalog', (req, res) => {
+    res.render('catalog', {
+        title: 'Справочник',
+        isCatalog: true
+    })
+})
+
+router.get('/autorization', function (req, res) {
+    query.connectionParameters = config.reportConnStr;      //connecting to localhost
+    var deviceArray = new Array();
+    var sqlstr = "sdfsfdfsdsfds";
+    query(sqlstr, function (err, rows, result) {
+        assert.equal(rows, result.rows);
+        for (var i = 0; i < rows.length; i++) {
+            var device = {};
+            device.name = rows[i].device;
+            device.value = rows[i].totalqtyout;
+            deviceArray.push(device);
+        }
+        res.render('d3t1', { deviceArray: deviceArray });
+    });
+});
 
 module.exports = router;
